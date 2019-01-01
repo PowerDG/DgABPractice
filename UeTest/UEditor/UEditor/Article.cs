5eb11f8d860de4836a000e91bf47f60ec9d18d39
帕瓦罗地瓜PowerDG <1049365046@qq.com>
2019-01-01 10:19:13 +0800

    UEditor初始
    UEditor

---
+++ UeTest/UEditor/UEditor/Article.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UEditor
+{
+    public class Article
+    {
+
+
+        //https://github.com/baiyunchen/UEditor.Core/blob/master/Docs/DotnetCore%E4%B8%AD%E7%9A%84%E4%BD%BF%E7%94%A8.md
+        [Key]
+        public int ID { get; set; }
+
+        [DisplayName("标题")]
+        public string Title { get; set; }
+        [DisplayName("描述")]
+        public string Description { get; set; }
+        [DisplayName("内容")]
+        public string Content { get; set; }
+        [DisplayName("评论数")]
+        public long CommentCount { get; set; }
+        [DisplayName("发表时间")]
+        public DateTime PublishOn { get; set; }
+        [DisplayName("其它字段")]
+        [NotMapped]
+        public string OtherField { get; set; }
+    }
+}
---
+++ UeTest/UEditor/UEditor/Startup.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using UEditor.Core;
+
+namespace UEditor
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddUEditorService();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+       Path.Combine(Directory.GetCurrentDirectory(), "upload")),
+                RequestPath = "/upload",
+                OnPrepareResponse = ctx =>
+                {
+                    ctx.Context.Response.Headers.Append("Cache-Control", "public,max-age=36000");
+                }
+            });
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
