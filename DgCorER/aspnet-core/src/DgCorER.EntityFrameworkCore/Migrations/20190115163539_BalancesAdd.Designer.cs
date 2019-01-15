205b4f65d58940b9f7c9168f846285c604d80199
帕瓦罗地瓜PowerDG <1049365046@qq.com>
2019-01-16 00:37:20 +0800

    update-database

--- DgCorER/aspnet-core/src/DgCorER.EntityFrameworkCore/Migrations/20190115163539_BalancesAdd.Designer.cs
+++ DgCorER/aspnet-core/src/DgCorER.EntityFrameworkCore/Migrations/20190115163539_BalancesAdd.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using DgCorER.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace DgCorER.Migrations
 {
     [DbContext(typeof(DgCorERDbContext))]
-    partial class DgCorERDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190115163539_BalancesAdd")]
+    partial class BalancesAdd
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DgCorER/aspnet-core/src/DgCorER.EntityFrameworkCore/Migrations/20190115163539_BalancesAdd.cs
@@ -0,0 +1,366 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace DgCorER.Migrations
+{
+    public partial class BalancesAdd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameIndex(
+                name: "IX_AbpUserLoginAttempts_TenancyName_UserNameOrEmailAddress_Result",
+                table: "AbpUserLoginAttempts",
+                newName: "IX_AbpUserLoginAttempts_TenancyName_UserNameOrEmailAddress_Res~");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_AbpNotificationSubscriptions_TenantId_NotificationName_EntityTypeName_EntityId_UserId",
+                table: "AbpNotificationSubscriptions",
+                newName: "IX_AbpNotificationSubscriptions_TenantId_NotificationName_Enti~");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_AbpNotificationSubscriptions_NotificationName_EntityTypeName_EntityId_UserId",
+                table: "AbpNotificationSubscriptions",
+                newName: "IX_AbpNotificationSubscriptions_NotificationName_EntityTypeNam~");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "AbpUserTokens",
+                nullable: false,
+                oldClrType: typeof(long))
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "AbpUsers",
+                nullable: false,
+                oldClrType: typeof(long))
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "AbpUserRoles",
+                nullable: false,
+                oldClrType: typeof(long))
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "AbpUserOrganizationUnits",
+                nullable: false,
+                oldClrType: typeof(long))
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "AbpUserLogins",
+                nullable: false,
+                oldClrType: typeof(long))
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "AbpUserLoginAttempts",
+                nullable: false,
+                oldClrType: typeof(long))
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "AbpUserClaims",
+                nullable: false,
+                oldClrType: typeof(long))
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "AbpUserAccounts",
+                nullable: false,
+                oldClrType: typeof(long))
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "AbpTenants",
+                nullable: false,
+                oldClrType: typeof(int))
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "AbpSettings",
+                nullable: false,
+                oldClrType: typeof(long))
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "AbpRoles",
+                nullable: false,
+                oldClrType: typeof(int))
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "AbpRoleClaims",
+                nullable: false,
+                oldClrType: typeof(long))
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "AbpPermissions",
+                nullable: false,
+                oldClrType: typeof(long))
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "AbpOrganizationUnits",
+                nullable: false,
+                oldClrType: typeof(long))
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "AbpLanguageTexts",
+                nullable: false,
+                oldClrType: typeof(long))
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "AbpLanguages",
+                nullable: false,
+                oldClrType: typeof(int))
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "AbpFeatures",
+                nullable: false,
+                oldClrType: typeof(long))
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "AbpEntityPropertyChanges",
+                nullable: false,
+                oldClrType: typeof(long))
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "AbpEntityChangeSets",
+                nullable: false,
+                oldClrType: typeof(long))
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "AbpEntityChanges",
+                nullable: false,
+                oldClrType: typeof(long))
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "AbpEditions",
+                nullable: false,
+                oldClrType: typeof(int))
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "AbpBackgroundJobs",
+                nullable: false,
+                oldClrType: typeof(long))
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "AbpAuditLogs",
+                nullable: false,
+                oldClrType: typeof(long))
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameIndex(
+                name: "IX_AbpUserLoginAttempts_TenancyName_UserNameOrEmailAddress_Res~",
+                table: "AbpUserLoginAttempts",
+                newName: "IX_AbpUserLoginAttempts_TenancyName_UserNameOrEmailAddress_Result");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_AbpNotificationSubscriptions_TenantId_NotificationName_Enti~",
+                table: "AbpNotificationSubscriptions",
+                newName: "IX_AbpNotificationSubscriptions_TenantId_NotificationName_EntityTypeName_EntityId_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_AbpNotificationSubscriptions_NotificationName_EntityTypeNam~",
+                table: "AbpNotificationSubscriptions",
+                newName: "IX_AbpNotificationSubscriptions_NotificationName_EntityTypeName_EntityId_UserId");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "AbpUserTokens",
+                nullable: false,
+                oldClrType: typeof(long))
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "AbpUsers",
+                nullable: false,
+                oldClrType: typeof(long))
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "AbpUserRoles",
+                nullable: false,
+                oldClrType: typeof(long))
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "AbpUserOrganizationUnits",
+                nullable: false,
+                oldClrType: typeof(long))
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "AbpUserLogins",
+                nullable: false,
+                oldClrType: typeof(long))
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "AbpUserLoginAttempts",
+                nullable: false,
+                oldClrType: typeof(long))
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "AbpUserClaims",
+                nullable: false,
+                oldClrType: typeof(long))
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "AbpUserAccounts",
+                nullable: false,
+                oldClrType: typeof(long))
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "AbpTenants",
+                nullable: false,
+                oldClrType: typeof(int))
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "AbpSettings",
+                nullable: false,
+                oldClrType: typeof(long))
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "AbpRoles",
+                nullable: false,
+                oldClrType: typeof(int))
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "AbpRoleClaims",
+                nullable: false,
+                oldClrType: typeof(long))
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "AbpPermissions",
+                nullable: false,
+                oldClrType: typeof(long))
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "AbpOrganizationUnits",
+                nullable: false,
+                oldClrType: typeof(long))
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "AbpLanguageTexts",
+                nullable: false,
+                oldClrType: typeof(long))
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "AbpLanguages",
+                nullable: false,
+                oldClrType: typeof(int))
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "AbpFeatures",
+                nullable: false,
+                oldClrType: typeof(long))
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "AbpEntityPropertyChanges",
+                nullable: false,
+                oldClrType: typeof(long))
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "AbpEntityChangeSets",
+                nullable: false,
+                oldClrType: typeof(long))
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "AbpEntityChanges",
+                nullable: false,
+                oldClrType: typeof(long))
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "AbpEditions",
+                nullable: false,
+                oldClrType: typeof(int))
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "AbpBackgroundJobs",
+                nullable: false,
+                oldClrType: typeof(long))
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "AbpAuditLogs",
+                nullable: false,
+                oldClrType: typeof(long))
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+        }
+    }
+}
